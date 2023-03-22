c1bf3698ba18180167b7be55bbed3fa08f4d80d3
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-03-21 19:50:53 -0500

    Creating selectors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import NucampLogo from "./app/assets/img/logo.png";
 import "./App.css";
+import Header from "./components/Header";
+import CampsitesList from "./features/campsites/CampSitesList";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <img src={NucampLogo} alt="nucamp logo" />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      I am ready for workshop!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
