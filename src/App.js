981f6734d9721b27785859d1ddd823c1b8afff8c
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-03-22 21:40:44 -0500

    create card list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import Footer from "./components/footer";
 
@@ -7,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,14 @@ export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-  return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// export const selectRandomCampsite = () => {
+//   return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// };
+
+export const selectCampsiteById = (id) => {
+  return CAMPSITES.find((campsite) => campsite.id === id);
+};
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
 };
